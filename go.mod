4fda9a5b607949a2c714a176e468f7312c36c04c
Ziyi Fan <fanzy618@gmail.com>
2019-05-27 22:32:00 +0800

    Add docker-compose

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/fanzy618/wsproxy
+
+go 1.12
+
+require github.com/gorilla/websocket v1.2.0
--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ wsaddr=${WS_ADDR:=0.0.0.0:1443}
 if [[ "$TYPE" = "server" ]]
 then
 echo "Exec /wsproxy/wsproxy -role $TYPE -a $wsaddr"
-exec /wsproxy/server -role=$TYPE -a $wsaddr
+exec /wsproxy/wsproxy -role=$TYPE -a $wsaddr
 
 elif [[ "$TYPE" = "client" ]]
 then
